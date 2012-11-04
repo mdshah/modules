27746c3355355167b7c8dfec9ab9dd092a4a30e7
unknown <jjs2195@columbia.edu>
2012-11-04 14:12:48 -0500

    imports

--- modules/NLPParsing/TextParser.java
+++ modules/NLPParsing/TextParser.java
@@ -6,6 +6,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import opennlp.tools.chunker.ChunkerME;
+import opennlp.tools.chunker.ChunkerModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.InvalidFormatException;
 /**
  * This class parses the given description of the course
@@ -37,13 +46,13 @@ public class TextParser {
 		} catch(IOException ex) {
 			ex.printStackTrace();
 		}
-		List<Sentence> sentenceList = new LinkedList<Sentence>();
+		List<Sentence> sentenceList = new LinkedList();
 		String[] sentences = sentenceDetector.sentDetect(desc);		
 		for(String s : sentences) {
 			String[] tokens = tokenizer.tokenize(s);
 			String[] posTags = tagger.tag(tokens);
 			String[] chunks = chunker.chunk(tokens, posTags);
-			List<String> lemma = new ArrayList<String>();
+			List<String> lemma = new ArrayList();
 			for(String str : tokens) {
 				lemma.add(str.toLowerCase());
 			}
